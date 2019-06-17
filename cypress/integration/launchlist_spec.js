831c9f8223ca3ef600541c7723c860825504f050
daanlouter <daan.louter@gmail.com>
2019-06-17 16:46:42 +0100

    Add Cypress 🌈

---
+++ cypress/integration/launchlist_spec.js
@@ -0,0 +1,8 @@
+import { launchlist } from "@flourish/cypress";
+
+// How to populate launchlist: https://github.com/kiln/flourish-cypress#launchlist-data-types
+describe("Launchlist", () => {
+  var data_layout = {};
+  var options = {};
+  launchlist(options, data_layout);
+});
